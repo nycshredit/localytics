f49f481ad90d72f9bf24949a8e99565875aa4bfc
Matt Restivo <matthew.restivo@gmail.com>
2015-02-26 16:55:25 -0500

    updated encryption

--- test.php
+++ test.php
@@ -1,29 +1,56 @@
 <?php
+
 /*
 include 'AES.php';
 
 $inputText = "uSAi4awJLRvBAUyD1lZ0iG8AovZ/xtjURZzVZLlM3LA=";
 $inputKey = "LOCALYTICSTRACK1";
 
-echo "made it!";
 $blockSize = 256;
 $aes = new AES($inputText, $inputKey, $blockSize);
-//$enc = $aes->encrypt();
+$enc = $aes->encrypt();
 $aes->setData($enc);
 $dec=$aes->decrypt();
 echo "still made it!";
-//echo "After encryption: ".$enc."<br/>";
+echo "After encryption: ".$enc."<br/>";
+echo "<<<<< does this match? >>>>>>>>"
+echo "<br/>".$inputTextEnc."<br/>"
 echo "After decryption: ".$dec."<br/>";
 */
+
+// test 3
+/*
+include 'AES.php';
+
+$inputTextEnc = "IG87EISz9fsKFnQYQrni+A==";
+$inputText = "mcconnella1979";
+$inputKey = "LOCALYTICSTRACK1";
+
+$blockSize = 128;
+$aes = new AES($inputText, $inputKey, $blockSize);
+$enc = $aes->encrypt();
+$aes->setData($enc);
+$aes->setData($inputText);
+$dec=$aes->decrypt();
+
+echo "After encryption: ".$enc." >>> does this match? <<< ".$inputTextEnc."<br/>";
+
+echo "After decryption: ".$dec."<br/>";
+*/
+
 
 // test 2
 
 $Pass = "LOCALYTICSTRACK1";
-$Clear = "mattrestivovip";        
-//$Clear = "uSAi4awJLRvBAUyD1lZ0iG8AovZ/xtjURZzVZLlM3LA=";
+
+$Clear = "mcconnella1979";
+$ClearEnc = "IG87EISz9fsKFnQYQrni+A==";
+
+$crypted = "IG87EISz9fsKFnQYQrni+A==";
+//$crpyted = "uSAi4awJLRvBAUyD1lZ0iG8AovZ/xtjURZzVZLlM3LA="; // mattrestivovip this doesn't work
 
 $crypted = fnEncrypt($Clear, $Pass);
-echo "Encrypred: ".$crypted."</br>";
+echo "Encrypred: ".$crypted." >>>>>>>?????<<<<<<<< ".$ClearEnc."</br>";
 
 $newClear = fnDecrypt($crypted, $Pass);
 echo "Decrypred: ".$newClear."</br>";        
@@ -33,8 +60,9 @@ function fnEncrypt($sValue, $sSecretKey)
     return rtrim(
         base64_encode(
             mcrypt_encrypt(
-                MCRYPT_RIJNDAEL_256,
-                $sSecretKey, $sValue, 
+                //MCRYPT_RIJNDAEL_256,
+                MCRYPT_RIJNDAEL_128,
+				$sSecretKey, $sValue, 
                 MCRYPT_MODE_ECB, 
                 mcrypt_create_iv(
                     mcrypt_get_iv_size(
@@ -51,7 +79,8 @@ function fnDecrypt($sValue, $sSecretKey)
 {
     return rtrim(
         mcrypt_decrypt(
-            MCRYPT_RIJNDAEL_256, 
+            //MCRYPT_RIJNDAEL_256, 
+			MCRYPT_RIJNDAEL_128,
             $sSecretKey, 
             base64_decode($sValue), 
             MCRYPT_MODE_ECB,
