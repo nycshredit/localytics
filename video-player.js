a80454d8297d4f9af5c44cc2cb4ff4fc26ffb35a
Eric Schneider <eschneider@nhl.com>
2015-02-26 10:37:57 -0500

    initial files

---
+++ video-player.js
@@ -0,0 +1,54 @@
+
+var vpGlobalSettings = {
+		"site": "www", 
+	    "lang": "en", 
+	    "playerType": "n2", 
+	    "pageName": "nhl:en:news:news", 
+	    "section": "news",
+	    "section2": "news", 
+	    "hierarchy": "news,playersafety", 
+	    "videoChannel": "inline article",
+	    "noTealium": true,
+		"fwSectionId": "nhl_us_nhl_videocenter_playersafetydecisions",
+		"skipAdTime": 5
+}
+
+var VideoPrototype = {
+	_currentVideo : null,
+	play : function(aVideo) {
+		this._currentVideo = aVideo;
+		jQuery('#videoContainer').empty();
+		jQuery('#details h1').html(aVideo.title);
+		jQuery('#details p').html(aVideo.desc);
+		jQuery('#details p.durationDate').html(aVideo.duration +'sec &nbsp;|&nbsp; ' + aVideo.pubDate);
+		
+		jQuery('#details p.tags').empty();
+		for (i = 0; i < aVideo.tags.length; i++) {
+			var aTag = aVideo.tags[i];
+			jQuery('#details p.tags').append(
+					'<a href="javascript:void(0)">' + aTag + '</a> ');
+		}
+
+		var settings = {
+			instanceId : aVideo.id,
+			containerId : "videoContainer",
+			endCallback : VideoPrototype.videoEnded
+		};
+		var options = {
+			playlist : [ aVideo.id ],
+			autostart : true
+		};
+		var settings = jQuery.extend({}, vpGlobalSettings, settings);
+		VideoCenter.setup(settings);
+		VideoCenter.play(options, settings.instanceId);
+	},
+	videoEnded : function() {
+		var indexOfLast = playlist
+				.indexOf(VideoPrototype._currentVideo);
+		var nextIndex = (indexOfLast + 1);
+		if (nextIndex < playlist.length) {
+			VideoPrototype.play(playlist[nextIndex]);
+		}
+	}
+
+}
---
+++ video-playlist.js
@@ -0,0 +1,94 @@
+// js file
+
+var playlist = [
+{
+	"id" : 759192,
+	"duration" : 46,
+	"desc" : "Anaheim Ducks forward Kyle Palmieri collects a juicy rebound and sweeps the puck past Calgary Flames goalie Jonas Hiller to go up 4-2 in the third. ",
+	"tags" : [ "ANA", "Anaheim Ducks", "Kyle Palmieri" ],
+	"groupId" : 3000,
+	"title" : "Palmieri beats Hiller on a juicy rebound",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/21/759192_es.jpg",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/21/759192_eb.jpg",
+	"pubDate" : "02/21/2015 00:15:58 AM"
+},
+{
+	"id" : 759101,
+	"duration" : 42,
+	"desc" : "Anaheim Ducks forward Ryan Getzlaf rips a shot that hits off the back boards and bounces off the leg of Calgary Flames goalie Jonas Hiller and into the net.",
+	"tags" : [ "ANA", "Anaheim Ducks", "Latest", "Original Content ",
+			"Ryan Getzlaf", "Unlucky Bounces" ],
+	"groupId" : 3000,
+	"title" : "Getzlaf's shot takes an unlucky bounce off Hiller and into the net",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759101_es.jpg",
+	"categoryId" : "1438",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759101_eb.jpg",
+	"pubDate" : "02/21/2015 23:33:18 PM",
+	"dailyPlaylistId" : "131,35,72,49"
+},
+{
+	"id" : 759076,
+	"duration" : 41,
+	"desc" : "Anaheim Ducks forward Francois Beauchemin chips a one-timer from the slot past Calgary Flames goalie Jonas Hiller to tie the game up at 2-2 in the third. ",
+	"tags" : [ "ANA", "Anaheim Ducks", "Francois Beauchemin" ],
+	"groupId" : 3000,
+	"title" : "Beauchemin beats Hiller from the slot ",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759076_es.jpg",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759076_eb.jpg",
+	"pubDate" : "02/21/2015 02:15:22 AM",
+	"dailyPlaylistId" : "35,72"
+},
+{
+	"id" : 759043,
+	"duration" : 78,
+	"desc" : "Colorado Avalanche defenseman Tyson Barrie and forward Gabriel Landeskog each score a goal 24 seconds apart to go up 3-1 in the third. ",
+	"tags" : [ "COL", "Colorado Avalanche", "Gabriel Landeskog",
+			"Original Content ", "Quick Strikes", "Tyson Barrie" ],
+	"groupId" : 3000,
+	"title" : "Avalanche score two goals in 24 seconds",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759043_es.jpg",
+	"categoryId" : "1438",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759043_eb.jpg",
+	"pubDate" : "02/22/2015 21:08:03 PM",
+	"dailyPlaylistId" : "35,72,49"
+},
+{
+	"id" : 759042,
+	"duration" : 68,
+	"desc" : "Blackhawks goalie Corey Crawford misplays a center ice shot from Avalanche defensemen Brad Stuart and lets the puck squeak in under his stick.",
+	"tags" : [ "Brad Stuart", "COL", "Colorado Avalanche",
+			"Corey Crawford", "Editors Picks", "Odd Goals" ],
+	"groupId" : 3000,
+	"title" : "Stuart's center ice shot squeaks past Crawford",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759042_es.jpg",
+	"categoryId" : "1425",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759042_eb.jpg",
+	"pubDate" : "02/21/2015 23:33:18 PM",
+	"dailyPlaylistId" : "35,72,49"
+},
+{
+	"id" : 759032,
+	"duration" : 58,
+	"desc" : "Mark Roe is joined by Los Angeles Kings forward Trevor Lewis, live from the 2015 Coors Light practice skate at Levi’s Stadium.",
+	"tags" : [ "LAK", "Los Angeles Kings", "Stadium Series",
+			"Trevor Lewis" ],
+	"groupId" : 3000,
+	"title" : "Lewis Stadium Series Interview",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759032_es.jpg",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759032_eb.jpg",
+	"pubDate" : "02/21/2015 04:03:19 AM",
+	"dailyPlaylistId" : "53,23"
+},
+{
+	"id" : 759020,
+	"duration" : 78,
+	"desc" : "Mark Roe is joined by Los Angeles Kings forward Tyler Toffoli, live from the 2015 Coors Light practice skate at Levi’s Stadium.",
+	"tags" : [ "LAK", "Los Angeles Kings", "Stadium Series",
+			"Tyler Toffoli" ],
+	"groupId" : 3000,
+	"title" : "Toffoli Stadium Series Interview",
+	"thumb" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759020_es.jpg",
+	"canvas" : "http://nhl.cdnllnwnl.neulion.net/u/www/thumbs/2015/02/20/759020_eb.jpg",
+	"pubDate" : "02/21/2015 04:03:19 AM",
+	"dailyPlaylistId" : "53,23"
+} ];
